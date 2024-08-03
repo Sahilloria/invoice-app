1f85cd33821e5e304f72d4f8d7049015e8e0ac36
Sahil <sahilloria83@gmail.com>
2024-08-03 15:42:58 +0100

    some work

--- Backend/src/db/connectDB.js
+++ Backend/src/db/connectDB.js
@@ -3,8 +3,8 @@ import { db_Name } from "../constants.js";
 
 export const connectDB=async () =>{
     try {
-        await mongoose.connect(`${process.env.MONGO_DB_URL}/${db_Name}`)
-        // console.log(res.connection.host)
+      const res=   await mongoose.connect(`${process.env.MONGO_DB_URL}/${db_Name}`)
+        console.log(res.connection)
     } catch (error) {
         console.log("Failed to connect DB",error)
     }
